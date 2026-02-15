2a612638d66ac19f03295a877548d5b504eb3356
PRANEETH MAHESHWARAN <praneeth.m1000@gmail.com>
2026-02-15 07:35:10 +0530

    organie

---
+++ dataset_generation/generate.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sdv.single_table import CTGANSynthesizer
+
+# ----------------------------
+# STEP 1: LOAD TRAINED MODEL
+# ----------------------------
+
+model = CTGANSynthesizer.load("dataset_generation\ctgan_model.pkl")
+
+print("Model loaded successfully!")
+
+# ----------------------------
+# STEP 2: GENERATE SYNTHETIC DATA
+# ----------------------------
+
+NUM_SAMPLES = 20000
+
+synthetic_data = model.sample(num_rows=NUM_SAMPLES)
+
+print(f"{NUM_SAMPLES} synthetic rows generated.")
+
+# ----------------------------
+# STEP 3: SAVE DATASET
+# ----------------------------
+
+synthetic_data.to_csv("datase.csv", index=False)
+
+print("Saved as datase.csv")
